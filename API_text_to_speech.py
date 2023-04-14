274213faa09f8fcf6f1cd4c34d981b98c915a8e9
karlchristiaens1 <34423435+karlchristiaens1@users.noreply.github.com>
2023-04-14 12:35:24 +0100

    Update API_text_to_speech.py

--- API_text_to_speech.py
+++ API_text_to_speech.py
@@ -1,13 +1,14 @@
 import BEMO_secrets as secrets
+
 def api_call(text, filename):
     import json
     from ibm_watson import TextToSpeechV1
     from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-
-    API_KEY = "dm5FOdVtAaQ6j9x64oLpbsYlA3JxhmACtIHpYmigFZKN"
-
+    
+    # API Service URL
     url = "https://api.eu-gb.text-to-speech.watson.cloud.ibm.com/instances/afe9327c-aa41-4216-ace5-92ed6f0b595d"
-
+    
+    # Calling Text-to-Speech API
     authenticator = IAMAuthenticator(secrets.IBM_TEXT_TO_SPEECH_APIKEY)
     text_to_speech = TextToSpeechV1(
         authenticator=authenticator
@@ -15,9 +16,9 @@ def api_call(text, filename):
 
     text_to_speech.set_service_url(url)
 
-    #Other interesting voices for a bear
-    #US_AllisonV3Voice
-    #en-US_KevinV3Voice
+    # Other interesting voices for a bear:
+    # US_AllisonV3Voice
+    # en-US_KevinV3Voice
     # voices = text_to_speech.list_voices().get_result()
     # print(json.dumps(voices, indent=2))
 
@@ -30,12 +31,10 @@ def api_call(text, filename):
                 accept='audio/wav'        
             ).get_result().content)
 
-    
-#spoken = 'Hi KARL there, can you tell me what medecine you have taken today and how you physically feel?'
-# print(spoken)
 
-# #Call text-to-speech(spoken) service
-#bemo_path = '/home/karlmarc/Desktop/BEMO/BEMO-main/'
-#api_call(spoken, bemo_path + 'spoken_1_test.wav')
+# FOR TESTING PURPOSES 
+# spoken = 'Hello there, can you tell me what medecine you have taken today and how you physically feel?'
+# bemo_path = '/home/karlmarc/Desktop/BEMO/BEMO-main/'
+# api_call(spoken, bemo_path + 'spoken_1_test.wav') 
 
     
