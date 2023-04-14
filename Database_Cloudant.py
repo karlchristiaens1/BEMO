6f5b4766bea17d2dcf37495f5b388ac40d25a38e
karlchristiaens1 <34423435+karlchristiaens1@users.noreply.github.com>
2023-04-14 12:43:52 +0100

    Update Database_Cloudant.py

--- Database_Cloudant.py
+++ Database_Cloudant.py
@@ -1,16 +1,8 @@
-# CRED = {
-#     "apikey": "Yavn5ukfgf1_0HHLsqJLDvunLSwsEuYp_-tymc3JhamA",
-#     "host": "bd4526e8-bde9-48ba-9d66-518274b7d624-bluemix.cloudantnosqldb.appdomain.cloud",
-#     "iam_apikey_description": "Auto-generated for key crn:v1:bluemix:public:cloudantnosqldb:eu-gb:a/fe59afbbc2664eaf8333e59c51e8a439:2b085d86-4e05-4d72-8121-00c2a3df1815:resource-key:574f22fc-6179-4d8f-88d4-fe0b5c5164b4",
-#     "iam_apikey_name": "Service credentials-1",
-#     "iam_role_crn": "crn:v1:bluemix:public:iam::::serviceRole:Manager",
-#     "iam_serviceid_crn": "crn:v1:bluemix:public:iam-identity::a/fe59afbbc2664eaf8333e59c51e8a439::serviceid:ServiceId-84605e58-5668-4149-9662-ee55d98dd401",
-#     "url": "https://bd4526e8-bde9-48ba-9d66-518274b7d624-bluemix.cloudantnosqldb.appdomain.cloud",
-#     "username": "bd4526e8-bde9-48ba-9d66-518274b7d624-bluemix"
-#     }
+
 import BEMO_secrets as secrets
 import raspi_ears_servo as rpes
-    # # Deleting the database.
+    
+    # DELETING A DATABASE
     # try :
     #     client.delete_database(databaseName)
     # except CloudantException:
@@ -20,8 +12,6 @@ import raspi_ears_servo as rpes
 
     # Closing the connection to the service instance.
     # client.disconnect()
-
-
 
 def created_DB(dB_name):
     from ibmcloudant.cloudant_v1 import CloudantV1, Document
@@ -77,6 +67,8 @@ def create_cm_doc(standardised_data):
     emotion_monitoring = standardised_data[0], #Input
     )
     response = service.post_document(db='continousmonitoring', document=products_doc).get_result()
+    
+    # TRIGGEREING BEMO'S EMOTIONS
     if emotion_monitoring['joy'] >= 0.6:
         rpes.thread_happy()
     elif emotion_monitoring['sadness'] >= 0.6:
@@ -90,14 +82,14 @@ def create_cm_doc(standardised_data):
     else:
         pass
 
-#Example Data for testing:
+# FOR TESTING PURPOSES:
 # fakedata = [{'SYMPTOM': [], 'MEDICATION': [], 'EFFECT': [{'effect': 'feel', 'specific': [{'labels': []}], 'status': []}]}, {'FOOD_DRINK': [{'healthy': [{'food': 'apple', 'status': [], 'time': ['lunch', 'TIME']}], 'unhealthy': [{'food': 'processed foods', 'status': ['LESS'], 'time': []}], 'other': []}], 'EFFECT': []}, {'exercise': [], 'status': [], 'time': []}, 'I feel like a six ', 'my day was fantastic I went out and did sports ']
 # fakedata2 = [{'sadness': 0.031585, 'joy': 0.027666, 'fear': 0.014837, 'disgust': 0.022785, 'anger': 0.907784}]
 
-#Uncomment to create a dB
+# Uncomment to create a dB
 # created_DB('continousmonitoring')
 
-#Uncomment the following to test creating a document for the two different dB
+# Uncomment the following to test creating a document for the two different dB
 # create_health_check_doc(fakedata)
 # create_cm_doc(fakedata2)
 
