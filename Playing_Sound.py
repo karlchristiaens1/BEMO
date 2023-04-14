add01e356cba66a1c4f037fb8a223774007da51d
karlchristiaens1 <34423435+karlchristiaens1@users.noreply.github.com>
2023-04-14 12:57:56 +0100

    Update Playing_Sound.py

--- Playing_Sound.py
+++ Playing_Sound.py
@@ -4,8 +4,6 @@ def audio_play(path):
     
     # for playing note.wav file
     playsound(path)
-    print('playing sound using  playsound') # "C:\Users\karlc\test1.wav"
-    
-#audio_play(path = '//home//pi//Desktop//BEMO//BEMO-main//command_play_podcast_4.wav')
 
-#/home/pi/Desktop/BEMO/BEMO-main/command_play_podcast_4.wav
+# FOR TESTING PURPOSES:    
+# audio_play(path = '//home//pi//Desktop//BEMO//BEMO-main//command_play_podcast_4.wav')
